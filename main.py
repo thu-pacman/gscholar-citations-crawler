80b78a4044db51922701e0c1169302d1e487ade3
Qianqian Shi <shiqiezi@gmail.com>
2016-01-17 23:40:34 -0800

    Fix #8 Exit program when google scholar complains that it is a robot

--- main.py
+++ main.py
@@ -26,9 +26,7 @@ def get_all_citations():
     for c in range(0, int(math.ceil(total_citations_num * 1.0 / papers_per_page))):
         paper_uri = paper_uri_template % (papers_per_page * c, papers_per_page)
         logging.info("Processing GOOGLE_SCHOLAR_URI: " + paper_uri)
-        req = urllib2.Request(paper_uri, headers=REQUEST_HEADERS)
-        page = urllib2.urlopen(req)
-        soup = BeautifulSoup(page)
+        soup = create_soup_by_url(paper_uri)
         paper_records = soup("tr", {"class":'gsc_a_tr'})
         for p in paper_records:
             paper_title = p.find('a', {"class":"gsc_a_at"}).getText()
@@ -45,36 +43,30 @@ def get_total_citations_num():
     """
     Get the total citation number from user's google scholar homepage
     """
-    req = urllib2.Request(myconfig.google_scholar_uri, headers=REQUEST_HEADERS)
-    page = urllib2.urlopen(req)
-    soup = BeautifulSoup(page)
+    soup = create_soup_by_url(myconfig.google_scholar_uri)
     total_citations_num = int(soup("td", {"class":"gsc_rsb_std"})[0].getText())
     logging.info("Total citations number: %d" % total_citations_num)
     return total_citations_num
 
 def get_citations_by_paper(citations_uri, count):
+    logging.debug("citations_uri: %s count: %d" % (citations_uri, count))
     citations_uri_template = citations_uri + "&start=%d"
     for c in range(0, int(math.ceil(count / 10.0))):
         curr_citations_uri = citations_uri_template % (c * 10)
-        logging.debug("Processing citations_uri:" + curr_citations_uri)
-        req = urllib2.Request(curr_citations_uri, headers=REQUEST_HEADERS)
-        page = urllib2.urlopen(req)
-        soup = BeautifulSoup(page)
+        logging.debug("Processing citations_uri: " + curr_citations_uri)
+        soup = create_soup_by_url(curr_citations_uri)
         for citation_record in soup('div', {"class":"gs_r"}):
             save_citation(citation_record)
-            time.sleep(SLEEP_INTERVAL)
 
 def save_citation(citation_record):
-    CITE_DETAIL_URL_TEMPLATE = "https://scholar.google.com/scholar?q=info:%(id)s:scholar.google.com/&output=cite&scirp=0"
+    CITE_DETAIL_URL_TEMPLATE = "https://scholar.google.com/scholar?q=info:%(id)s:scholar.google.com/&output=cite"
     cite_anchor = citation_record.find('a', {'class' : 'gs_nph', 'href' : '#', "role":"button"})
     if not cite_anchor:
         return
     citation_id = cite_anchor['onclick'].split(',')[1][1:-1]
     cite_detail_url = CITE_DETAIL_URL_TEMPLATE % {"id" : citation_id}
     logging.info("Getting formated cite from " + cite_detail_url)
-    req = urllib2.Request(cite_detail_url, headers = REQUEST_HEADERS)
-    page = urllib2.urlopen(req)
-    soup = BeautifulSoup(page)
+    soup = create_soup_by_url(cite_detail_url)
     global html_parser
     full_cite = html_parser.unescape(soup.find("div", {"id":"gs_cit0"}).text)
     global citation_num
@@ -96,10 +88,28 @@ def download_pdf(pdf_uri):
         download_num = download_num + 1
         logging.info("Downloaded citation [%d] from link %s " % (citation_num, pdf_uri))
     except urllib2.URLError as err:
-        logging.error("Can't download link: " + pdf_uri + " Error: " +str(err.reason))
+        logging.error("Can't download link: " + pdf_uri + " Error: " + str(err.reason))
     finally:
         if pdf:
             pdf.close()
+
+
+def create_soup_by_url(page_url):
+    req = urllib2.Request(page_url, headers = REQUEST_HEADERS)
+    page = None
+    try:
+        time.sleep(SLEEP_INTERVAL)
+        page = urllib2.urlopen(req)
+        soup = BeautifulSoup(page)
+        if soup.h1 and soup.h1.text and soup.h1.text == "Please show you&#39;re not a robot":
+            raise Exception("You need to verify manually that you're not a robot.")
+        return soup
+    except Exception as err:
+        logging.error("Can't open link: " + page_url + " Error: " + str(err))
+        sys.exit(1)
+    finally:
+        if page:
+            page.close()
 
 def main():
     reload(sys)
